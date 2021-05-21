549cc7df9d092f3a52fb5f7dd1881ab921e3cd6f
Cat <morancathy@gmail.com>
2021-05-21 00:04:21 -0500

    spensor trys to drink water

--- js/script.js
+++ js/script.js
@@ -6,7 +6,8 @@ Tamagotchi
 /* ======================
 CACHED DOM NODES
 =========================*/
-
+const menuEl = document.querySelector('.what-to-do-menu');
+const asidePEl = document.querySelector('.aside > p');
 
 /* ======================
 CREATE Spensor and Master Troll
@@ -19,11 +20,11 @@ class Hero {
     this.sanity = 5;
     this.coconuts = 0;
     this.bamboo = 0;
-    this.rope = 0 ;
+    this.rope = 0;
   }
 
   drink(){
-
+    this.water += 1;
   }
 
   eat(){
@@ -61,49 +62,89 @@ class Hero {
   buildShelter() {
 
   }
-
 }
 
+const Spensor = new Hero("Spensor");
 
 
 /* ======================
 GLOBAL VARS
 =========================*/
+let coconuts = (Math.floor(Math.random() * (3-1) + 1));
+let bamboo = (Math.floor(Math.random() * (6-3) + 3));
+let rope = (Math.floor(Math.random() * (3-1) + 1));
 
 
 /* =============================
 FUNCTIONS
 ============================= */
+
 displayWhatToDoToday = () => {
-  document.querySelector('.aside > p').textContent = "What should Spensor do today?";
+  document.querySelector('.aside > p').textContent = "What should Spensor do today?";  //best practice to do this or create a variable?
 
-  const foodButton = document.createElement('button'); //do i have to do these 5 lines for each button?
-  foodButton.setAttribute('id', 'food-button');
-  foodButton.innerHTML = 'Search for food';
-  const menuEl = document.querySelector('.what-to-do-menu');
-  menuEl.appendChild(foodButton);
+  let toDoButtons = [
+    {text: 'Seach for water', id: 'water-button'},
+    {text: 'Seach for food', id: 'food-button'},
+    {text: 'Seach for supplies', id: 'supplies-button'},
+    ];
+  for(let bananas of toDoButtons){
+    const buttonEl = document.createElement('button');
+    buttonEl.setAttribute('id', bananas.id);
+    buttonEl.innerHTML = bananas.text;
+    menuEl.appendChild(buttonEl);
+  }
+}
+displayWhatToDoToday();    //this gets called at begingin of each day
 
-  const waterButton = document.createElement('button');
-  waterButton.setAttribute('id', 'water-button');
-  waterButton.innerHTML = 'Search for water';
-  menuEl.appendChild(waterButton);
+searchForWater = () => {
+  //remove the "what do you want to do today questions and buttons"
+  //new scene of Spensor walking across beach/forest in search, takes maybe 5 seconds?
+  //text on screen "you found x coconuts" while page shows spensor next to the item celebrating
+  alert(`You found ${coconuts} cocounts!`);
 
-  const suppliesButton = document.createElement('button');
-  suppliesButton.setAttribute('id', 'supplies-button');
-  suppliesButton.innerHTML = 'Search for supplies';
-  menuEl.appendChild(suppliesButton);
+  //New question on screen "What would you like to do with it?"
+  document.querySelector('.aside > p').textContent = "What would you like to do with the coconut?";
+  //Buttons pop up with text "Drink"  "Save for later"
+  const buttonEl = document.createElement('button');
+  buttonEl.setAttribute('id', 'drink-button');
+  buttonEl.innerHTML = "Drink";
+  asidePEl.appendChild(buttonEl);
+  console.log(document.querySelector('#drink-button')) //checker
+
+  const buttonEl2 = document.createElement('button');
+  buttonEl2.setAttribute('id', 'save-for-later-button');
+  buttonEl2.innerHTML = "Save for later";
+  asidePEl.appendChild(buttonEl2);
+
+  //side bar notation says "Coconuts can be used to defend yourself against nasty trolls" (But only the first time he comes across coconuts)
+
+  //if he clicks drink then his water levels go up one.
+
+  //if he saves, his coconut invintory goes up one.
+}
+
+searchForFood = () => {
 
 }
-displayWhatToDoToday()    //this gets called at begingin of each day
 
-// selectingWhatToDoToday(){
-//
-// }
+searchForSupplies = () => {
 
+}
 
 /* =============================
 EVENT LISTENERS
 ============================= */
+document.querySelector('#water-button').addEventListener('click', searchForWater);
+document.querySelector('#food-button').addEventListener('click', searchForFood);
+document.querySelector('#supplies-button').addEventListener('click',searchForSupplies);
+document.querySelector('#drink-button').addEventListener('click', Spensor.drink); //why doesnt this work?
+// document.querySelector('#save-for-later-button'). addEventListener('click',       )
 
 
-console.log("word up")
+
+
+
+
+
+
+console.log("word up");
